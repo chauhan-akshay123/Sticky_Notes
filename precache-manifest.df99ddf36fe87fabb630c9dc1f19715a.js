6e5349c94f333160ed8af921964d2ee6a4f5c7b1
Akshay Chauhan <chauhanakshay395@gmail.com>
2023-07-01 18:12:21 +0530

    Updates

---
+++ precache-manifest.df99ddf36fe87fabb630c9dc1f19715a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7bda5ed86fde3fdee8a25288ecd8b36",
+    "url": "/Sticky_Notes/index.html"
+  },
+  {
+    "revision": "cb392f285f8f34c506d9",
+    "url": "/Sticky_Notes/static/js/2.b0d4c692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sticky_Notes/static/js/2.b0d4c692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4341f8bc1057f38662be",
+    "url": "/Sticky_Notes/static/js/main.2d02ba3e.chunk.js"
+  },
+  {
+    "revision": "b0715639dccc4b7fea60",
+    "url": "/Sticky_Notes/static/js/runtime-main.763734e5.js"
+  }
+]);
